b39b0fe767327106f04ceb8f63d825a6208964ce
Kali <kali@kali>
2022-12-08 16:58:39 -0500

    added sonar

--- script.groovy
+++ script.groovy
@@ -1,5 +1,12 @@
 def git_clone(){
     git "https://github.com/Medaliza/spring-devops-project"
 }
+def run_unit_tests(){
+    sh "mvn clean"
+    sh "mvn test"
+}
+def sonarqube_scan(){
+    sh "mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=oussema"
 
+}
 return this
